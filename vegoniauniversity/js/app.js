433432cb505ac3e85ae3dbc0807ef1eaf68b2945
Aungnaingphyo <aungnaingphyo00@gmail.com>
2022-09-27 00:37:10 +0630

    26 sep 2022

---
+++ vegoniauniversity/js/app.js
@@ -0,0 +1,95 @@
+
+
+// // Start Jquery Area
+
+$(document).ready(function () {
+
+    // Start Header
+    //  Start Nav Bar
+
+    $(".navbuttons").click(function () {
+        $(this).toggleClass("crossxs");
+    });
+    // End Nav Bar
+
+    // End Header
+
+});
+
+
+
+
+// End jQuery Area
+
+// Start Javascript Area
+
+// Start Student Counter Section
+
+var getcountervalues = document.querySelectorAll('.countervalues');
+
+console.log(getcountervalues);
+
+getcountervalues.forEach(function (getcountervalue) {
+    getcountervalue.textContent = 0;
+
+    // console.log(getcountervalue);
+
+    const updatecounter = function () {
+        // console.log('i am working');
+
+        const getcttarget = +getcountervalue.getAttribute('data-target');
+        // console.log(getcttarget);
+        // console.log(typeof getcttarget, getcttarget);
+
+        const getctcontent = +getcountervalue.innerText;
+        console.log(getctcontent, getctcontent);
+
+        const incnumber = getcttarget / 40;
+        console.log(incnumber);
+
+        if (getctcontent < getcttarget) {
+            getcountervalue.textContent = getctcontent + incnumber;
+            setTimeout(updatecounter, 50);
+        }
+    }
+
+    updatecounter();
+});
+
+// End Student Counter Section
+
+
+//Start Rating Section
+
+//start google code for chart
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Myanmar',     8],
+          ['Thailand',      2],
+          ['Singapore',  4],
+          ['Indonesia', 2],
+          ['Sirilanks',    8]
+        ]);
+
+        var options = {
+        //   title: 'My Daily Activities',
+		//   is3D: true
+		width:550,
+		height:440
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+
+//end google code for chart
+
+//End Rating Section
+
+// End Javascript Area 
